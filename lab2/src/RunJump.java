d600da2e858e58fc02dcaea969d4a558377bee9f
MaximBerdnikov <152172319+MaximBerdnikov@users.noreply.github.com>
2023-12-04 11:57:43 +0300

    Delete lab2 directory

--- lab2/src/RunJump.java
+++
@@ -1,7 +0,0 @@
-public interface RunJump {
-    public void run(int rundist);
-    public void jump(double jumpdist);
-    public double getMaxjump();
-    public int getMaxrundist();
-
-}
--- lab2/src/Treadmill.java
+++
@@ -1,9 +0,0 @@
-public class Treadmill extends Obstructions {
-    int Lenght;
-    public Treadmill(int lenght){
-        this.Lenght=lenght;
-    }
-    public int getdist(){
-        return Lenght;
-    }
-}
